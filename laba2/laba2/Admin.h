965048f84b0358eb6b7294d18862ef6251019e3f
Ivan <i.chernyadev.ivan.i@gmail.com>
2021-12-18 18:12:57 +0300

    4 Задание

--- laba2/laba2/Admin.h
+++ laba2/laba2/Admin.h
@@ -1,6 +1,7 @@
 //Приходит бабка в европесйкий магазин и говорит дай манки
 
 #pragma once
+#include <string>
 #include "iostream"
 #include "DefaultOperations.h"
 class Admin: public DefaultOperations
@@ -9,6 +10,15 @@ protected:
 	std::string firstname_;
 	std::string	lastname_;
 	std::string	category_;
+
+bool IsNumber (string strrr) {
+
+	return (strrr.Replace ("0", "").Replace ("1", "").Replace ("2", "").Replace
+("3", "").Replace ("4", "").Replace ("5", "").Replace ("6", "").Replace
+("7", "").Replace ("8", "").Replace ("9", "").Length == 0);
+
+}
+
 
 	void editProduct();
 	void deleteProduct();
